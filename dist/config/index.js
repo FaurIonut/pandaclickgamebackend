6530d455824c39230693372461552236149b49dc
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-25 01:32:32 +0300

    .

--- dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    mongoURI: "mongodb+srv://walter:nEObatlD6OcCVGaF@walter.iiya7fr.mongodb.net/mike-token-game",
-    jwtSecret: "kut10developers",
-    githubToken: "<yoursecrectaccesstoken>",
-};
--- dist/middleware/requireAdmin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
-function default_1(req, res, next) {
-    if (!req.user || req.user.role !== "admin") {
-        return res.status(403).json({ msg: "Access denied" }); // Changed status to 403 (Forbidden)
-    }
-    next();
-}
